7c9d318c24468d65313ff8f906373740fa4ef445
yuanjianqiang01 <13381072303@163.com>
2019-08-29 10:14:33 +0800

    添加了一个普通方法

--- src/com/cn/Test01.java
+++ src/com/cn/Test01.java
@@ -1,6 +1,12 @@
 package com.cn;
 
 public class Test01 {
+
+    public void getNum(){
+        System.out.println("this is first change");
+    }
+
+
     public static void main(String[] args) {
         System.out.println(
                 "hello world!"
